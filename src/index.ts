14dcc874b3059890be3dc06b5a9d3c31dfbfe7aa
tim.oberlaender <tim.oberlaender@stud.uni-goettingen.de>
2021-11-28 23:26:29 +0100

    modified:   map.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,40 @@ bootstrapExtra().catch(e => console.error(e));
 let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
+WA.state.saveVariable("link", "https://opentogethertube.com/room/BrvchFloor"); ///
+
 
 WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+    currentPopup =  WA.ui.openPopup("clockPopup","hier kannst du entscheiden ob die musik über youtube laufen soll oder über den stream server, dann kannst du auflegen",[
+        {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    },
+        {
+        label: "stream",
+            className: "primary",
+            callback: (popup) => {
+                WA.room.setProperty('audio', 'openWebsite',undefined);    
+                WA.room.setProperty('audio', 'playAudio',"http://localhost:8000/example");    
+            }
+        
+        },
+        {
+            label: "youtube",
+                className: "primary",
+                callback: (popup) => {
+                    // Close the popup when the "Close" button is pressed.
+                    WA.room.setProperty('audio', 'openWebsite', 'https://opentogethertube.com/room/BrvchFloor'); 
+                    WA.room.setProperty('audio', 'playAudio',undefined);    
+                       
+                }
+            
+            }
+]);
 })
 
 WA.room.onLeaveZone('clock', closePopUp)
